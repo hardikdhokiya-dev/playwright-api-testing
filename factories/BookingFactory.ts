f18a489175aa05dae777714d5949ee4b3bed7ef9
HARDIK DHOKIYA <dhokiya_h@outlook.in>
2026-07-06 23:10:17 -0400

    feat: structure test framework layout with docs, factories, interfaces, and utils

---
+++ factories/BookingFactory.ts
@@ -0,0 +1,43 @@
+import { BookingRequest } from "../interfaces/Booking";
+
+export class BookingFactory {
+
+	static createBooking ( override ?: Partial<BookingRequest> ) : BookingRequest {
+
+		const defaultBooking : BookingRequest = {
+
+			firstname : "Jofra",
+			lastname : "Archer",
+			totalprice : 300,
+			depositpaid : true,
+			
+			bookingdates : {
+				checkin : "2026-07-01",
+				checkout : "2026-07-10"
+			},
+
+			additionalneeds: "Breakfast"
+
+		};
+
+		return {
+			... defaultBooking,
+			... override
+		};
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+  
+}
