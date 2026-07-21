5eaaa7bfe973f837300d530836eac94b5a81a150
HARDIK DHOKIYA <dhokiya_h@outlook.in>
2026-07-20 22:53:37 -0400

    added TC_01

--- tests/booking.get.spec.ts
+++ tests/booking.get.spec.ts
@@ -1,7 +1,10 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, APIResponse } from "@playwright/test";
 import { BookingClient } from "../clients/BookingClient";
 import { BookingHelper } from "../utils/BookingHelper";
 import { CreateBooking } from "../interfaces/CreateBooking";
+import { request } from "node:http";
+import { BookingAssertions } from "../assertions/BookingAssertions";
+import { ResponseAssertions } from "../assertions/ResponseAssertions";
 
 test.describe("GET /booking", () => {
 
@@ -17,18 +20,31 @@ test.describe("GET /booking", () => {
      * Verify an existing booking can be retrieved by booking ID.
      */
 
-    test("should retrieve booking by valid booking id", { tag: ["@api", "@booking", "@get", "@regression"] }, async () => {
+    test("should retrieve booking by valid booking id", { tag: ["@api", "@booking", "@get", "@regression"] }, async ({request}) => {
+
+        let createdBooking: CreateBooking;
+        let response : APIResponse;
+
+        await test.step("Create a booking", async () => {
+            createdBooking = await BookingHelper.createBooking(request);
+        });
+        
+
+        await test.step("Retrieve booking by id", async () => {
+            response = await bookingClient.getBooking(createdBooking.bookingId);
+        });
 
 
+        await test.step("Validate booking", async () => {
 
+                BookingAssertions.expectBookingCreated(response, createdBooking.booking);
 
+                ResponseAssertions.expectStatus(response, 200);
 
+                ResponseAssertions.expectSuccess(response)
 
-
-
-
-
-
+            });
+        
     });
 
 
